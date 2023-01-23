6399f74f63c9a2a24c63f150a6f00d8cdebbb001
Erich Heine <sophacles@gmail.com>
2023-01-23 08:50:49 -0600

    Tests for edge cases

--- src/separated_list/list_state.rs
+++ src/separated_list/list_state.rs
@@ -7,7 +7,9 @@ use serde_derive::{Deserialize, Serialize};
 ///
 /// This state tracks the selected item in a list, and provides methods for cycling the list.
 /// The size of the list is the number of [`ListItem`](super::ListItem)s to cycle through.
-#[derive(Debug, Default)]
+///
+/// panics if created or resized to have a size of 0
+#[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct ListState {
     pub(super) size: usize,
@@ -18,7 +20,11 @@ pub struct ListState {
 impl ListState {
     /// Create a new state for a list of length `size`.
     pub fn new(size: usize) -> Self {
-        let mut res = Self::default();
+        let mut res = ListState {
+            size: 1,
+            selected: 0,
+            window_first: 0,
+        };
         res.resize(size);
         res
     }
@@ -66,9 +72,83 @@ impl ListState {
 
     /// set the number of [ListItems](super::ListItem) in the list.
     pub fn resize(&mut self, size: usize) {
+        if size == 0 {
+            panic!("ListState has invalid size: 0");
+        }
         self.size = size;
         if self.selected >= self.size {
             self.selected = self.size.saturating_sub(1);
         }
+    }
+}
+
+impl Default for ListState {
+    fn default() -> Self {
+        ListState::new(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cycles() {
+        let mut s = ListState::new(3);
+        assert_eq!(s.selected(), 0);
+        s.cycle_next();
+        assert_eq!(s.selected(), 1);
+        s.cycle_next();
+        assert_eq!(s.selected(), 2);
+        s.cycle_next();
+        assert_eq!(s.selected(), 0);
+
+        s.cycle_prev();
+        assert_eq!(s.selected(), 2);
+        s.cycle_prev();
+        assert_eq!(s.selected(), 1);
+        s.cycle_prev();
+        assert_eq!(s.selected(), 0);
+    }
+
+    #[test]
+    fn movement() {
+        let mut s = ListState::new(3);
+        assert_eq!(s.selected(), 0);
+        s.prev();
+        assert_eq!(s.selected(), 0);
+        s.next();
+        assert_eq!(s.selected(), 1);
+        s.next();
+        assert_eq!(s.selected(), 2);
+        s.next();
+        assert_eq!(s.selected(), 2);
+    }
+
+    #[test]
+    fn resize() {
+        let mut s = ListState::new(3);
+        s.select(2);
+        assert_eq!(s.selected(), 2);
+        s.resize(2);
+        assert_eq!(s.selected(), 1);
+        s.resize(4);
+        assert_eq!(s.selected(), 1);
+        s.cycle_prev();
+        s.cycle_prev();
+        assert_eq!(s.selected(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn zero_size_create() {
+        let _ = ListState::new(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn zero_size_resize() {
+        let mut s = ListState::new(1);
+        s.resize(0);
     }
 }
